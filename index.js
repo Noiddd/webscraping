b5bf62b4165ba69d0a2645493e556154ee46d586
Dion Ang <dion@Dions-MacBook-Pro.local>
2023-02-08 23:27:38 +0800

    test

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ async function start(){
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.goto("https://www.mycareersfuture.gov.sg/search?search=remote&sortBy=relevancy&page=0")
-    await page.screenshot({path:"amazing.png", fullPage: true})
     await browser.close()
 }
 
